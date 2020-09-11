967caa46829b9808a8458498e58a059fd750b45f
liangyawang <874934797@qq.com>
2020-09-11 16:57:49 +0800

    add scheduler backfile

--- myapp/scheduler/views.py
+++ myapp/scheduler/views.py
@@ -5,14 +5,15 @@ from django.template import loader
 from .models import scheduler_table
 from django.http import Http404
 
-def index(requests):
-    container_list = scheduler_table.objects.all()
-    print(container_list)
-    template = loader.get_template('scheduler/index.html')
-    context = {
-        'container_list':container_list,
-    }
-    return HttpResponse(template.render(context,requests))
+def memberadd(request):
+    return HttpResponse('ok')
+
+
+    #template = loader.get_template('scheduler/index.html')
+    # context = {
+    #     'container_list':container_list,
+    # }
+    # return HttpResponse(template.render(context,requests))
 
 
 
